13cd195859ea9a1712de0972c75ab2d1220312f1
pasheehan9 <163068901+pasheehan9@users.noreply.github.com>
2024-03-31 21:29:21 -0400

    Finished Week 3 Exercises

---
+++ R Project/r_project_code.R
@@ -0,0 +1,6 @@
+
+#Loading in Salary Data
+
+salary <- read.csv("C:/DSE5002/R Project/patricksheehan.module05RProject.csv"
+                             ,stringsAsFactors=FALSE
+                    )
