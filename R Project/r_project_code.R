06417eaae83c77ffde3ef0fc8b7f410c03b1f5d8
pasheehan9 <163068901+pasheehan9@users.noreply.github.com>
2024-04-11 22:24:12 -0400

    Finishing up Project and starting Python

--- R Project/r_project_code.R
+++ R Project/r_project_code.R
@@ -1,3 +1,7 @@
+----------------------
+##Loading Packages in
+----------------------
+
 library(dplyr)
 library(ggplot2)
 library(stringr)
@@ -26,43 +30,101 @@ print(unique_job_titles)
 
 salary <- salary[salary$company_size != 'L', ]
 
------------------------------------
-##Removing Freelance and Part Time
------------------------------------
+--------------------------------------------
+##Removing Freelance and Part Time Employees
+--------------------------------------------
   
-median_df <-salary[salary$employment_type %in% c('CT','FT'), ]
+foreign_median_df <-salary[salary$employment_type %in% c('CT','FT'), ]
 
-
-median_df <-median_df %>%
-  mutate(foreign_residence = median_df$employee_residence != 'US')
+----------------------------------------------------
+##Adding in a Column to show foreign vs US Residency
+-----------------------------------------------------
+  
+foreign_median_df <-foreign_median_df %>%
+  mutate(foreign_residence = foreign_median_df$employee_residence != 'US')
 
 --------------------------------
 ##Adding in median salary column
 --------------------------------
   
-median_df <-median_df %>%
+foreign_median_df <-foreign_median_df %>%
   group_by(experience_level, employment_type, foreign_residence) %>%
   mutate(median_salary = median(salary_in_usd))
 
+-----------------------------------------------------------------------
+##Making Experience Level/Remote Ratio more Readable in both Data frames
+------------------------------------------------------------------------
+salary <- salary %>%
+  mutate(experience_level = recode(experience_level, EN = 'Entry Level', MI = 'Mid-level', SE =  'Senior Level', EX = 'Executive Level' ))
 
------------------------------------------
-##Creating Experience Level Vector to Order
------------------------------------------
+salary$remote_ratio = factor(salary$remote_ratio, 
+                     levels = c(0, 50, 100), 
+                     labels = c("On-Site", "Hybrid", "Remote"))
+
+foreign_median_df <- foreign_median_df %>%
+  mutate(experience_level = recode(experience_level, EN = 'Entry Level', MI = 'Mid-level', SE =  'Senior Level', EX = 'Executive Level' ))
+
+
+foreign_median_df$remote_ratio = factor(foreign_median_df$remote_ratio, 
+                      levels = c(0, 50, 100), 
+                      labels = c("On-Site", "Hybrid", "Remote"))
+
+------------------------------------------------
+##Recoding employment type for display purposes
+------------------------------------------------
+
+foreign_median_df <- foreign_median_df %>%
+  mutate(employment_type = recode(employment_type, FT = 'Full Time', CT = 'Contract', FL =  'Freelance' ))
   
-level_order <- c('EN', "MI", "SE", 'EX') 
+-------------------------------------------
+##Creating Experience Level Vector to Order
+-------------------------------------------
+  
+level_order <- c('Entry Level', "Mid-level", "Senior Level", 'Executive Level') 
 
 --------------------------------------------------
-  ##Chart to display US v Foreign Median Salaries
+##Chart to display US v Foreign Median Salaries
 --------------------------------------------------
 
-ggplot(median_df)+
+ggplot(foreign_median_df)+
   geom_point((aes( x= factor(experience_level, level = level_order), y= median_salary, color = foreign_residence, size = 2))) +
   facet_wrap(.~employment_type) +
-  scale_x_discrete(level_order) +
-  scale_y_continuous(labels = scales::dollar_format(), limits = c(0,450000))+
-  labs(x= "Experience Level", y = "Median Salary in USD", title = "Median Salary for Contract/Fulltime Workers Grouped By Residence")  +
+  scale_y_continuous(labels = scales::dollar_format(), limits = c(0,450000), breaks = c(0,50000,100000,150000,200000,250000,300000,350000,400000,450000))+
+  labs(x= "Experience Level", y = "Median Salary in USD", title = "Median Salary for Contract/Fulltime in Data Jobs",color = "Foreign Residence?")  +
   theme(axis.text.x = element_text(angle = 45)) +
   scale_size_continuous(guide = "none")
+
+---------------------------------------------------------------------------------------
+##Creating Data frame to show change in Salary of selected Data Science Roles over time
+---------------------------------------------------------------------------------------
+  
+all_data_scientists <- salary[salary$job_title %in% c('Data Scientist','Data Analyst','Data Engineer'), ]
+
+##Narrowing to only contract/Full time
+
+all_data_scientists <- all_data_scientists[all_data_scientists$employment_type %in% c('CT','FT'),]
+
+##Narrowing to only mid/senior level employees
+
+all_data_scientists <- all_data_scientists[all_data_scientists$experience_level %in% c('Senior Level','Mid-level'),]
+
+##Adding in median salary column to dataframe grouped by work year, experience level, and job title
+
+all_data_scientists<- all_data_scientists %>%
+  group_by(work_year, experience_level, job_title) %>%
+  mutate(median_salary = median(salary_in_usd))
+
+----------------------------------------------------
+## Graph of Changing Salaries over time by job title
+----------------------------------------------------
+  
+ggplot(all_data_scientists) +
+  geom_line(aes(x = work_year, y = median_salary, color = job_title, group = job_title)) +
+  theme(axis.text.x = element_text(angle = 45)) +
+  facet_wrap(.~experience_level) +
+  scale_x_continuous(breaks = c(2020,2021,2022)) +
+  scale_y_continuous(labels = scales::dollar_format(),breaks = c(0,25000,50000,75000,100000,125000,150000),limits = c(0,150000)) +
+  labs(x= "Work Year", y = "Median Salary in USD", title = "Median Data Scientist Salaries Over Time",color = 'Job Title') 
 
 --------------------------------------------------
 ##Creating Data frame for Data Scientist in the US
@@ -72,7 +134,7 @@ us_data_scientist <- salary[salary$employee_residence == 'US', ]
 
 ##Narrowing to only Senior Level
 
-us_data_scientist_senior <- us_data_scientist[us_data_scientist$experience_level == 'SE', ]
+us_data_scientist_senior <- us_data_scientist[us_data_scientist$experience_level == 'Senior Level', ]
 
 ##Narrowing to only Data Scientists
 
@@ -80,16 +142,31 @@ us_data_scientist_senior_title <- us_data_scientist_senior[us_data_scientist_sen
 
 ##Creating Data Frames for on-site vs remote
 
-us_data_scientist_senior_title_on_site <- us_data_scientist_senior_title[us_data_scientist_senior_title$remote_ratio == 0, ]
+us_data_scientist_senior_title_on_site <- us_data_scientist_senior_title[us_data_scientist_senior_title$remote_ratio == 'On-Site', ]
 
-us_data_scientist_senior_title_remote <-us_data_scientist_senior_title[us_data_scientist_senior_title$remote_ratio != 0, ]
+us_data_scientist_senior_title_remote <-us_data_scientist_senior_title[us_data_scientist_senior_title$remote_ratio == 'Remote', ]
 
+--------------------------------------------------------------------------------------------
+##Finding Percentiles for breakdown on Salaries in USD for remote vs on site Data Scientists
+--------------------------------------------------------------------------------------------
 
 percentile_75_on_site <- quantile(us_data_scientist_senior_title_on_site$salary_in_usd, .75)
 percentile_50_on_site <- quantile(us_data_scientist_senior_title_on_site$salary_in_usd, .50)
+percentile_25_on_site <- quantile(us_data_scientist_senior_title_on_site$salary_in_usd, .25)
 
 percentile_75_remote <- quantile(us_data_scientist_senior_title_remote$salary_in_usd, .75)
 percentile_50_remote <- quantile(us_data_scientist_senior_title_remote$salary_in_usd, .50)
+percentile_25_remote <- quantile(us_data_scientist_senior_title_remote$salary_in_usd, .25)
+
+-----------------------------------------------------------------
+##Box Plot Showing Percentiles of salary in USD of Data Scientist
+-----------------------------------------------------------------
+  
+ggplot(us_data_scientist_senior_title) +
+  geom_boxplot(aes(x=job_title, y= salary_in_usd, fill = remote_ratio)) +
+  scale_y_continuous(labels = scales::dollar_format(), breaks = c(0,106763,130000,145200,172500,210000,250000)) +
+  facet_grid(.~remote_ratio) +
+  labs(x= "Job Title", y = "Salary in USD", title = "Box Plot of Salary for Senior Level US Data Scientists", fill = "Remote or On-Site") 
 
 -----------------------------------------------
 ##Creating Data frame for Foreign Data Engineer
@@ -97,109 +174,68 @@ percentile_50_remote <- quantile(us_data_scientist_senior_title_remote$salary_in
   
 foreign_data_engineer <- salary[salary$employee_residence != 'US', ]
 
-##Narrowing to only Senior Level
+##Narrowing to only Mid-level
 
-foreign_data_engineer_mid <- foreign_data_engineer[foreign_data_engineer$experience_level == 'MI', ]
+foreign_data_engineer_mid <- foreign_data_engineer[foreign_data_engineer$experience_level == 'Mid-level', ]
 
-##Narrowing to only Data Scientists
+##Narrowing to only Data Engineers
 
 foreign_data_engineer_mid_title <- foreign_data_engineer_mid[foreign_data_engineer_mid$job_title == 'Data Engineer', ]
 
 ##Creating Data Frames for remote
 
-foreign_data_engineer_senior_title_remote <-foreign_data_engineer_mid_title[foreign_data_engineer_mid_title$remote_ratio == 100, ]
+foreign_data_engineer_senior_title_remote <-foreign_data_engineer_mid_title[foreign_data_engineer_mid_title$remote_ratio == 'Remote', ]
+
+--------------------------------------------------------------------------------------------
+##Finding Percentiles for breakdown on Salaries in USD for remote and foreign Data Engineers
+--------------------------------------------------------------------------------------------
 
 percentile_75_remote_engineer <- quantile(foreign_data_engineer_senior_title_remote$salary_in_usd, .75)
 percentile_50_remote_engineer <- quantile(foreign_data_engineer_senior_title_remote$salary_in_usd, .50)
 percentile_25_remote_engineer <- quantile(foreign_data_engineer_senior_title_remote$salary_in_usd, .25)
--------------------------------------
-##Show Percentiles of Data Scientist
--------------------------------------
 
-ggplot(us_data_scientist_senior_title) +
-  geom_boxplot(aes(x=job_title, y= salary_in_usd)) +
-  scale_y_continuous(labels = scales::dollar_format(), breaks = c(0,100000,130000,145200,172500,210000, 250000)) +
-  facet_grid(.~remote_ratio) +
-  labs(x= "Job Title", y = "Salary in USD", title = "Salary for Senior Level US Data Scientists") 
-
-----------------------------------------
-##Show Box Plot of Foreign Data Engineer
-----------------------------------------
+------------------------------------------------------------------------
+##Box Plot Showing Percentiles of salary in USD of Foreign Data Engineer
+------------------------------------------------------------------------
   
 ggplot(foreign_data_engineer_senior_title_remote) +
-  geom_boxplot(aes(x=job_title, y= salary_in_usd)) +
-  scale_y_continuous(labels = scales::dollar_format(), breaks = c(0,40000,60658,76940,87932,240000)) +
+  geom_boxplot(aes(x=job_title, y= salary_in_usd,fill = job_title)) +
+  scale_y_continuous(labels = scales::dollar_format(), breaks = c(0,25000,50000,60658,76940,87932,100000,125000,150000)) +
   facet_grid(.~remote_ratio) +
-  labs(x= "Job Title", y = "Salary in USD", title = "Salary for Foreign Mid-Level Data Engineer") 
+  labs(x= "Job Title", y = "Salary in USD", title = "Box Plot of Salary for Foreign Mid-Level Data Engineer", fill = "Job Title") 
 
-
--------------------------------------
-##Show Percentiles of Data Analyst
--------------------------------------
+----------------------------------------------
+##Creating Data Frame for Foreign Data Analyst
+----------------------------------------------
   
-ggplot(us_data_scientist_senior_title) +
-  geom_boxplot(aes(x=job_title, y= salary_in_usd)) +
-  scale_y_continuous(labels = scales::dollar_format(), breaks = c(0,100000,130000,145200,172500,210000, 250000)) +
+foreign_data_analyst <- salary[salary$employee_residence != 'US', ]
+
+##Narrowing to only Mid-level
+
+foreign_data_analyst_mid <- foreign_data_analyst[foreign_data_analyst$experience_level == 'Mid-level', ]
+
+##Narrowing to only Data Analysts
+
+foreign_data_analyst_mid_title <- foreign_data_analyst_mid[foreign_data_analyst_mid$job_title == 'Data Analyst', ]
+
+##Creating Data Frames for remote
+
+foreign_data_analyst_title_remote <-foreign_data_analyst_mid_title[foreign_data_analyst_mid_title$remote_ratio == 'Remote', ]
+
+--------------------------------------------------------------------------------------------
+##Finding Percentiles for breakdown on Salaries in USD for remote and foreign Data Analysts
+--------------------------------------------------------------------------------------------
+
+percentile_75_remote_analyst <- quantile(foreign_data_analyst_title_remote$salary_in_usd, .75)
+percentile_50_remote_analyst <- quantile(foreign_data_analyst_title_remote$salary_in_usd, .50)
+percentile_25_remote_analyst <- quantile(foreign_data_analyst_title_remote$salary_in_usd, .25)
+ 
+------------------------------------------------------------------------
+##Box Plot Showing Percentiles of salary in USD for Foreign Data Analyst
+------------------------------------------------------------------------
+  
+ggplot(foreign_data_analyst_title_remote) +
+  geom_boxplot(aes(x=job_title, y= salary_in_usd,fill = job_title)) +
+  scale_y_continuous(labels = scales::dollar_format(), breaks = c(0,25000,32974,39263,43966,50000)) +
   facet_grid(.~remote_ratio) +
-  labs(x= "Job Title", y = "Salary in USD", title = "Salary for Senior Level US Data Scientists") 
-
-
-
-
-
-
-
---------------------------------------------
-##Show Change in Salary of Data Scientists
---------------------------------------------
-all_data_scientists <- salary[salary$job_title == 'Data Scientist', ]
-
-all_data_scientists <- all_data_scientists[all_data_scientists$employment_type %in% c('CT','FT'),]
-
-all_data_scientists<- all_data_scientists %>%
-  group_by(work_year, experience_level) %>%
-  mutate(median_salary = median(salary_in_usd))
-  
-ggplot(all_data_scientists) +
-  geom_line(aes(x = work_year, y = median_salary, color = experience_level, group = experience_level)) +
-  theme(axis.text.x = element_text(angle = 45)) +
-  scale_x_continuous(breaks = c(2020,2021,2022)) +
-  scale_y_continuous(labels = scales::dollar_format()) +
-  labs(x= "Work Year", y = "Median Salary in USD", title = "Median Data Scientist Salaries Over Time") 
-
---------------------------------------------
-##Show Change in Salary of Data Engineers
---------------------------------------------
-all_data_engineers <- salary[salary$job_title == 'Data Engineer', ]
-
-all_data_engineers <- all_data_engineers[all_data_scientists$employment_type %in% c('CT','FT'),]
-
-all_data_engineers<- all_data_engineers %>%
-  group_by(work_year, experience_level) %>%
-  mutate(median_salary = median(salary_in_usd))
-
-ggplot(all_data_engineers) +
-  geom_line(aes(x = work_year, y = median_salary, color = experience_level, group = experience_level)) +
-  theme(axis.text.x = element_text(angle = 45)) +
-  scale_x_continuous(breaks = c(2020,2021,2022)) +
-  scale_y_continuous(labels = scales::dollar_format(), limits = c(0,150000),breaks = c(0, 50000,78526,100000,130000,150000)) +
-  labs(x= "Work Year", y = "Median Salary in USD", title = "Median Data Engineer Salaries Over Time") 
-
---------------------------------------------
-##Show Change in Salary of Data Analysts
----------------------------------------------
-all_data_analysts <- salary[salary$job_title == 'Data Analyst', ]
-
-all_data_analysts <- all_data_analysts[all_data_analysts$employment_type %in% c('CT','FT'),]
-
-all_data_analysts<- all_data_analysts %>%
-  group_by(work_year, experience_level) %>%
-  mutate(median_salary = median(salary_in_usd))
-
-ggplot(all_data_analysts) +
-  geom_line(aes(x = work_year, y = median_salary, color = experience_level, group = experience_level)) +
-  theme(axis.text.x = element_text(angle = 45)) +
-  scale_x_continuous(breaks = c(2020,2021,2022)) +
-  scale_y_continuous(labels = scales::dollar_format(), limits = c(0,150000)) +
-  labs(x= "Work Year", y = "Median Salary in USD", title = "Median Data Analyst Salaries Over Time") 
-
+  labs(x= "Job Title", y = "Salary in USD", title = "Box Plot of Salary for Foreign Mid-Level Data Analyst", fill = "Job Title") 
--- Week_5/Examples/ex1.py
+++ Week_5/Examples/ex1.py
@@ -10,3 +10,31 @@ import numpy as np
 df = load_pandas().data
 df.columns
 np.round(np.mean(df['1963']),2)
+
+
+from statsmodels.datasets.interest_inflation.data import load_pandas
+df = load_pandas().data
+df.head()
+
+import statsmodels.api as sm
+import numpy as np
+df
+Y = df['Dp']
+X = df['R']
+x = sm.add_constant(X)
+model = sm.OLS(Y,X)
+results = model.fit()
+results.summary()
+
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+from statsmodels.datasets.interest_inflation import load_pandas
+df = load_pandas().data
+model = smf.quantreg('df ~ R',df)
+res = model.fit(q=.5)
+
+res2_coefs = res.params
+
+print(res2_coefs)
+
+print(results.summary())
